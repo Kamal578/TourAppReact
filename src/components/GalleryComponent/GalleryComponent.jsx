95ad08b173987e50dfda8e5c8e32339e64ca4083
MirHeyder03 <mhyahyayev03@gmail.com>
2022-10-08 15:51:16 +0400

    contact section added

--- src/components/GalleryComponent/GalleryComponent.jsx
+++ src/components/GalleryComponent/GalleryComponent.jsx
@@ -6,9 +6,10 @@ import img3 from "../../assets/img/gallery/gallery3.png";
 import img4 from "../../assets/img/gallery/gallery4.png";
 import img5 from "../../assets/img/gallery/gallery5.png";
 import img6 from "../../assets/img/gallery/gallery6.png";
-import {AiOutlineClose} from 'react-icons/ai'
-import {GrLinkNext, GrLinkPrevious} from 'react-icons/gr'
-
+import { AiOutlineClose } from "react-icons/ai";
+import { GrLinkNext, GrLinkPrevious } from "react-icons/gr";
+import { MdArrowBackIos } from "react-icons/md";
+import { MdArrowForwardIos } from "react-icons/md";
 function GalleryComponent() {
   let Data = [
     { id: 1, imgSrc: img1 },
@@ -18,22 +19,57 @@ function GalleryComponent() {
     { id: 5, imgSrc: img5 },
     { id: 6, imgSrc: img6 },
   ];
-  const [data,setData] = useState({img: '',index:0});
+  const [data, setData] = useState({ img: "", i: 0 });
   const [model, setModel] = useState(false);
   const [tempImgSrc, setTempImg] = useState("");
+
   const getImg = (imgSrc) => {
     setTempImg(imgSrc);
     setModel(true);
   };
-  const imgAction = ()=>{
-
-  }
+  const imgAction = (action) => {
+    let i = data.i;
+    if (action === "next-img") {
+      setData({ img: Data[i + 1], i: i + 1 });
+    }
+    if (action === "previous-img") {
+      setData({ img: Data[i - 1], i: i - 1 });
+    }
+  };
 
   return (
     <>
       <div className={model ? "model open" : "model"}>
+        <button
+          className="imgLeftArrow"
+          onClick={() => imgAction("previous-img")}
+        >
+          <MdArrowBackIos
+            className="icon"
+            style={{
+              position: "absolute",
+              top: "20px",
+              left: "20px",
+            }}
+          />
+        </button>
         <img src={tempImgSrc} alt="tempImg" />
-        <AiOutlineClose onClick={()=>{setModel(false)}}/>
+        <button className="imgRightArrow" onClick={() => imgAction("next-img")}>
+          <MdArrowForwardIos
+            className="icon"
+            style={{
+              position: "absolute",
+              top: "20px",
+              right: "20px",
+            }}
+          />
+        </button>
+
+        <AiOutlineClose
+          onClick={() => {
+            setModel(false);
+          }}
+        />
       </div>
       <div className="flex flex-wrap items-center justify-center cursor-pointer">
         {Data.map((item, index) => {
