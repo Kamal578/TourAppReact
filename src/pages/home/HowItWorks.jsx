56ca785a53cedaa913182bae35859cef817c9a56
Ahmadov Kamal <ahmadov.kamal423@gmail.com>
2022-10-09 18:49:29 +0400

    some minor changes

--- src/pages/home/HowItWorks.jsx
+++ src/pages/home/HowItWorks.jsx
@@ -5,32 +5,32 @@ import PaperPlane from "../../assets/icons/OutlinePaperPlane.svg";
 
 function HowItWorks() {
   return (
-    <div className="bg-howItWorks relative w-screen h-[50vh] bg-no-repeat bg-cover bg-center max-w-full">
-      <div className="absolute w-full h-full top-0 bg-whiteGradient text-7xl bg-white bg-opacity-50 flex flex-col gap-2">
+    <div className="bg-howItWorks relative w-screen h-[80vh] bg-no-repeat bg-cover bg-center max-w-full">
+      <div className="absolute w-full h-full top-0 bg-whiteGradient text-7xl bg-white bg-opacity-80 flex flex-col justify-center gap-20">
         <p className="font-primary text-7xl text-center mt-10 font-bold text-darkerGreen">
           How it works?
         </p>
         <div className="flex flex-row justify-between items-center px-[5rem]">
           <div
             id="howItWorksEl1"
-            className="flex flex-col flex-wrap items-center justify-center"
+            className="flex flex-col flex-wrap items-center justify-between"
           >
-            <img src={OutlineFileSearch} className="w-[5rem]" alt="notepad" />
-            <p className="text-2xl">Explore Tours</p>
+            <img src={OutlineFileSearch} className="w-[6rem]" alt="notepad" />
+            <p className="text-2xl text-greyish font-primary font-normal">Explore Tours</p>
           </div>
           <div
             id="howItWorksEl2"
-            className="flex flex-col items-center justify-center"
+            className="flex flex-col items-center justify-between"
           >
-            <img src={PaperPlane} className="w-[5rem]" alt="paperplane" />
-            <p className="text-2xl">Find your tours</p>
+            <img src={PaperPlane} className="w-[6rem]" alt="paperplane" />
+            <p className="text-2xl text-greyish font-primary font-normal">Find your tours</p>
           </div>
           <div
             id="howItWorksEl2"
-            className="flex flex-col items-center justify-center"
+            className="flex flex-col items-center justify-between"
           >
-            <img src={CloudCheck} className="w-[5rem]" alt="paperplane" />
-            <p className="text-2xl">Start booking</p>
+            <img src={CloudCheck} className="w-[6rem]" alt="paperplane" />
+            <p className="text-2xl text-greyish font-primary font-normal">Start booking</p>
           </div>
         </div>
       </div>
