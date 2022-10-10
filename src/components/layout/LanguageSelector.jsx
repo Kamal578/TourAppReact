56157b0bb5d9a0cdcf2f103c758c4f6daef17592
MirHeyder03 <mhyahyayev03@gmail.com>
2022-10-11 01:40:37 +0400

    LanguageSelector small change

--- src/components/layout/LanguageSelector.jsx
+++ src/components/layout/LanguageSelector.jsx
@@ -3,13 +3,18 @@ import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 export default function LanguageSelector() {
   return (
-    <div className="hover:cursor-pointer">
-      <select className="pr-10 text-xl font-bold text-white bg-transparent border-transparent rounded appearance-none w-35 border-1 hover:border-gray-400 focus:outline-none hover:cursor-pointer ">
-        <option value={"eng"} className="text-black bg-transparent">Eng</option>
-        <option value={"aze"} className="text-black bg-transparent">Aze</option>
-        <option value={"rus"} className="text-black bg-transparent">Rus</option>
+    <div className="">
+      <select className="flex items-center pr-1 text-xl text-white bg-transparent border-white size-4">
+        <option value="eng" className="text-black bg-transparent">
+          Eng
+        </option>
+        <option value="aze" className="text-black bg-transparent">
+          Aze
+        </option>
+        <option value="rus" className="text-black bg-transparent">
+          Rus
+        </option>
       </select>
-      <MdOutlineKeyboardArrowDown size={'25px'} className="relative text-white left-14 -top-6"/>
     </div>
   );
 }
