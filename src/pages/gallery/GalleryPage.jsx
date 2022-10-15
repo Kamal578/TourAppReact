81fbb05e9befd0e00bc34388a8ef6758e0759910
Ahmadov Kamal <ahmadov.kamal423@gmail.com>
2022-10-15 18:16:07 +0400

    ?

--- src/pages/gallery/GalleryPage.jsx
+++ src/pages/gallery/GalleryPage.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Header from '../../components/layout/Header'
+import Footer from '../../components/layout/Footer'
+import Hero from './Hero'
 
 export default function Gallery() {
   return (
-    <div>Gallery</div>
+    <div>
+      <Header/>
+      <Hero/>
+      <Footer/>
+    </div>
   )
 }
