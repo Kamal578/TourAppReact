7c9839620db0adb521a68d89829b9884db79a768
Ahmadov Kamal <ahmadov.kamal423@gmail.com>
2022-10-27 10:28:33 +0400

    how it works fix

--- src/pages/home/HowItWorks.jsx
+++ src/pages/home/HowItWorks.jsx
@@ -7,30 +7,48 @@ function HowItWorks() {
   return (
     <div className="bg-howItWorks relative w-screen h-[80vh] bg-no-repeat bg-cover bg-center max-w-full">
       <div className="absolute w-full h-full top-0 bg-whiteGradient text-7xl bg-white bg-opacity-80 flex flex-col justify-center gap-20">
-        <p className="font-primary text-7xl text-center mt-10 font-bold text-darkerGreen">
+        <p className="text-4xl md:text-7xl text-center mt-10 font-bold text-darkerGreen">
           How it works?
         </p>
-        <div className="flex flex-row justify-between items-center px-[5rem]">
+        <div className="flex flex-row justify-between items-center px-5 md:px-[5rem]">
           <div
             id="howItWorksEl1"
             className="flex flex-col flex-wrap items-center justify-between"
           >
-            <img src={OutlineFileSearch} className="w-[6rem]" alt="notepad" />
-            <p className="text-2xl text-greyish font-primary font-normal">Explore Tours</p>
+            <img
+              src={OutlineFileSearch}
+              className="md:w-[6rem] w-[4rem]"
+              alt="notepad"
+            />
+            <p className="text-2xl text-greyish   font-normal text-center">
+              Explore Tours
+            </p>
           </div>
           <div
             id="howItWorksEl2"
             className="flex flex-col items-center justify-between"
           >
-            <img src={PaperPlane} className="w-[6rem]" alt="paperplane" />
-            <p className="text-2xl text-greyish font-primary font-normal">Find your tours</p>
+            <img
+              src={PaperPlane}
+              className="md:w-[6rem] w-[4rem]"
+              alt="paperplane"
+            />
+            <p className="text-2xl text-greyish text-center">
+              Find your tours
+            </p>
           </div>
           <div
             id="howItWorksEl2"
             className="flex flex-col items-center justify-between"
           >
-            <img src={CloudCheck} className="w-[6rem]" alt="paperplane" />
-            <p className="text-2xl text-greyish font-primary font-normal">Start booking</p>
+            <img
+              src={CloudCheck}
+              className="md:w-[6rem] w-[4rem]"
+              alt="paperplane"
+            />
+            <p className="text-2xl text-greyish font-normal text-center">
+              Start booking
+            </p>
           </div>
         </div>
       </div>
@@ -38,5 +56,4 @@ function HowItWorks() {
   );
 }
 
-// style={{color: 'white', background: "linear-gradient(172.95deg, rgba(255, 255, 255, 0.2) 5.5%, rgba(255, 255, 255, 0.2) 94.69%), rgba(255, 255, 255, 0.87);"}}
 export default HowItWorks;
