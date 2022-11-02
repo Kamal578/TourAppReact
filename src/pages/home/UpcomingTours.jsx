d5d4afac13b922c8a4d6f753a340f1a403ad71a6
MirHeyder03 <mhyahyayev03@gmail.com>
2022-11-02 21:18:39 +0400

    upcoming tour fixed

--- src/pages/home/UpcomingTours.jsx
+++ src/pages/home/UpcomingTours.jsx
@@ -19,52 +19,22 @@ export default function UpcomingTours() {
     };
     getAllTours();
   }, []);
-  // const TourInfoExample = [
-  //   {
-  //     thumbnail: Baku,
-  //     title: "Baku City Tour",
-  //     date: "01-08.10.2022",
-  //     time: "10:00 - 23:00",
-  //     price: 100,
-  //   },
-  //   {
-  //     thumbnail: Sheki,
-  //     title: "Baku, Sheki",
-  //     date: "01-08.10.2022",
-  //     time: "10:00 - 23:00",
-  //     price: 100,
-  //   },
-  //   {
-  //     thumbnail: Guba,
-  //     title: "Quba,Shamakhi",
-  //     date: "01-08.10.2022",
-  //     time: "10:00 - 23:00",
-  //     price: 100,
-  //   },
-  //   {
-  //     thumbnail: Gusar,
-  //     title: "Quba, Qusar",
-  //     date: "01-08.10.2022",
-  //     time: "10:00 - 23:00",
-  //     price: 100,
-  //   },
-  // ];
   return (
     <div className="flex flex-col items-center justify-center my-10 bg-white h-100">
       <p className="mb-10 text-5xl font-bold text-center max-xss:text-3xl md:text-7xl text-darkerGreen font-primary">
         Upcoming Tours
       </p>
       <div className="flex flex-col flex-wrap items-center justify-center gap-x-3 md:flex-row">
-        {tours.map((tour) => (
+        {/* {tours.map((tour) => (
           <Tour
-            key={tour.title}
-            title={tour.title}
-            date={tour.date}
-            time={tour.time}
-            thumbnail={tour.thumbnail}
-            price={tour.price}
+            key={""}
+            title={""}
+            date={""}
+            time={""}
+            thumbnail={""}
+            price={""}
           />
-        ))}
+        ))} */}
       </div>
       <div className="pt-10 text-2xl font-semibold transition-all duration-200 border-2 border-transparent text-darkerGreen font-primary hover:border-b-darkerGreen">
         <NavLink exact to="/tours">
