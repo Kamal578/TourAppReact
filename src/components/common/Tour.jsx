0e5ce770450c2c9818dba9c351a1d9a1e103fc50
MirHeyder03 <mhyahyayev03@gmail.com>
2022-11-11 11:46:35 +0400

    tour updated

--- src/components/common/Tour.jsx
+++ src/components/common/Tour.jsx
@@ -2,30 +2,30 @@ import React from "react";
 import { AiOutlineCalendar, AiOutlineClockCircle } from "react-icons/ai";
 import { NavLink } from "react-router-dom";
 
-const Tour = (props) => {
+const Tour = ({ thumbnail, title, date, price }) => {
   const yellowStyle = { color: "#E8A908" };
   return (
     <div className="flex flex-col justify-center items-left">
       <div
         id="tourImage"
-        style={{ backgroundImage: `url(${props.thumbnail})` }}
+        style={{ backgroundImage: `url(${thumbnail})` }}
         className="border-transparent bg-cover bg-center rounded-lg md:px-36 w-full h-[70vh] m-2"
       ></div>
       <div className="flex flex-col items-left gap-y-4">
         <p className="text-3xl font-semibold text-left font-primary text-darkerGreen">
-          {props.title}
+          {title}
         </p>
         <p className="flex flex-row gap-2 text-2xl font-normal text-left text-black font-primary">
           <AiOutlineCalendar style={yellowStyle} />
-          {props.date}
+          {date}
         </p>
         <p className="flex flex-row justify-between gap-2 text-2xl font-normal text-left text-black font-primary">
           <span className="flex flex-row gap-2">
             <AiOutlineClockCircle style={yellowStyle} />
-            {props.time}
+            {time}
           </span>
           <span style={yellowStyle} className="font-medium">
-            {props.price}$
+            {price}$
           </span>
         </p>
         <NavLink
